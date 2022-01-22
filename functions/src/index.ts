05ac659fc45e691357b85e2c8dff1ce95fbdd178
mengxyz <meng348@gmail.com>
2022-01-22 18:15:19 +0700

    [changed] fully migrate node app to supabase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,22 @@
 "use strict";
 import * as express from "express";
-// import { Template } from "./template";
-// import axios, { Method } from "axios";
+import { Template } from "./template";
+import axios, { Method } from "axios";
 import * as fs from "fs";
 import * as cors from "cors";
 import * as apicache from "apicache";
 import rateLimit from "express-rate-limit";
-// import { TemplateNotFound } from "./exception";
-// import { createClient } from "@supabase/supabase-js";
+import { TemplateNotFound } from "./exception";
+import { createClient } from "@supabase/supabase-js";
+import * as dotenv from "dotenv";
 
-// const SUPABASE_API_URL = process.env.SUPABASE_API_URL || "";
-// const SUPABASE_KEY = process.env.SUPABASE_KEY || "";
+dotenv.config();
+
+const SUPABASE_API_URL = process.env.SUPABASE_API_URL || "";
+const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY || "";
 const port = 9000;
 
-// const client = createClient(SUPABASE_API_URL, SUPABASE_KEY);
+const client = createClient(SUPABASE_API_URL, SUPABASE_SECRET_KEY);
 const server = express();
 const versionPath = __dirname + "/../src/resource/version";
 const builddatePath = __dirname + "/../src/resource/builddate";
@@ -39,60 +42,61 @@ server.get("/", (_, res) => {
   });
 });
 
-// server.all("/:template/*", async (req, res) => {
-//   try {
-//     const doc = await db.ref("template").child(req.params.template).get();
-//     if (!doc.exists()) {
-//       throw new TemplateNotFound(req.params.template);
-//     }
-//     const template: Template = <Template>doc.toJSON();
-//     const path = req.path.replace(`/${req.params.template}/`, "");
-//     const url = `${template.baseUrl}${path}`;
-//     const query: Record<string, string> = {};
-//     const header: Record<string, string> = {};
+server.all("/:template/*", async (req, res) => {
+  try {
+    const doc = await client.from("template").select("*").eq("uid", req.params.template);
+    console.log(doc)
+    if (doc.data == null || doc.data.length == 0) {
+      throw new TemplateNotFound(req.params.template);
+    }
+    const template: Template = <Template>doc.data[0];
+    const path = req.path.replace(`/${req.params.template}/`, "");
+    const url = `${template.baseUrl}${path}`;
+    const query: Record<string, string> = {};
+    const header: Record<string, string> = {};
 
-//     Object.assign(query, req.query, template.params);
-//     Object.assign(header, template.headers);
+    Object.assign(query, req.query, template.params);
+    Object.assign(header, template.headers);
 
-//     console.info({
-//       template_id: req.params.template,
-//       method: req.method,
-//       path: path,
-//       header: req.headers,
-//       query: req.query,
-//       url: url,
-//     });
+    console.info({
+      template_id: req.params.template,
+      method: req.method,
+      path: path,
+      header: req.headers,
+      query: req.query,
+      url: url,
+    });
 
-//     const result = await axios({
-//       method: req.method as Method,
-//       headers: template.headers,
-//       url: url,
-//       params: template.params,
-//     });
+    const result = await axios({
+      method: req.method as Method,
+      headers: template.headers,
+      url: url,
+      params: template.params,
+    });
 
-//     if (result.headers["Content-type"] || result.headers["content-type"]) {
-//       res.set(
-//         "Content-type",
-//         result.headers["Content-type"] ?? result.headers["content-type"]
-//       );
-//     }
+    if (result.headers["Content-type"] || result.headers["content-type"]) {
+      res.set(
+        "Content-type",
+        result.headers["Content-type"] ?? result.headers["content-type"]
+      );
+    }
 
-//     console.log(result.headers);
-//     res.status(result.status).send(result.data);
-//   } catch (err) {
-//     console.log(err);
-//     if (err instanceof TemplateNotFound) {
-//       res.status(404).json({ message: err.message });
-//     } else {
-//       res.status(404).json({ message: "Not Found" });
-//     }
-//   }
-// });
+    console.log(result.headers);
+    res.status(result.status).send(result.data);
+  } catch (err) {
+    console.log(err);
+    if (err instanceof TemplateNotFound) {
+      res.status(404).json({ message: err.message });
+    } else {
+      res.status(404).json({ message: "Not Found" });
+    }
+  }
+});
 
 server.use((_, res) => {
   res.status(404).json({ message: "Not Found" });
 });
 
 server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("\x1b[36m",`Live on http://127.0.0.1:${port}`);
 });
