9aba2d93c99f32e3fd3507ea5e9cc5111d2b2dbf
mengxyz <meng348@gmail.com>
2022-02-21 00:27:08 +0700

    [added] use jwt token to delete user

--- functions/src/controller/delete-user.controller.ts
+++ functions/src/controller/delete-user.controller.ts
@@ -1,12 +1,25 @@
 import { supabaseService } from "./../services/supabase.service";
 import { Request, Response } from "express";
-import { UserNotFound } from "../exception";
+import * as jwt from "njwt";
+import { TokenExpired, UserNotFound } from "../exception";
 
-export default async function (req: Request, res: Response) {
+export default async function (req: Request, res: Response): Promise<void> {
   try {
-    await supabaseService.deleteUser(req.params.userId);
+    const token = jwt.verify(
+      req.headers.authorization?.split(" ")[1] ?? "",
+      process.env.JWT_SECRET
+    );
+    if (token?.isExpired() == true) {
+      throw new TokenExpired();
+    }
+    const userId = token?.body.toJSON()["sub"] as string | undefined;
+    await supabaseService.deleteUser(userId!);
     res.sendStatus(204);
-  } catch (error) {
+  } catch (error: any) {
+    if (error.name == "JwtParseError" || error instanceof TokenExpired) {
+      res.status(403).json({ message: "Unauthorized" });
+      return;
+    }
     if (error instanceof UserNotFound) {
       res.status(404).json({ message: error.message });
       return;
